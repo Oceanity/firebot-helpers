5c68567194a10b356b8af78ab2b38da1e68dd51f
Izzy Deane <github@oceanity.net>
2024-08-13 20:50:08 -0400

    Added twitchApi and twitchChat

--- lib/firebot/modules.ts
+++ lib/firebot/modules.ts
@@ -1,24 +1,28 @@
 import { ScriptModules } from "@crowbartools/firebot-custom-scripts-types";
 import { HttpServerManager } from "@crowbartools/firebot-custom-scripts-types/types/modules/http-server-manager";
 
-export let logger: ScriptModules["logger"];
-export let effectRunner: ScriptModules["effectRunner"];
 export let effectManager: ScriptModules["effectManager"];
+export let effectRunner: ScriptModules["effectRunner"];
 export let eventManager: ScriptModules["eventManager"];
 export let httpServer: HttpServerManager;
-export let variableManager: ScriptModules["replaceVariableManager"];
 export let integrationManager: ScriptModules["integrationManager"];
 export let jsonDb: unknown;
+export let logger: ScriptModules["logger"];
+export let twitchApi: ScriptModules["twitchApi"];
+export let twitchChat: ScriptModules["twitchChat"];
 export let utils: ScriptModules["utils"];
+export let variableManager: ScriptModules["replaceVariableManager"];
 
 export function initModules(scriptModules: ScriptModules) {
-  logger = scriptModules.logger;
-  effectRunner = scriptModules.effectRunner;
   effectManager = scriptModules.effectManager;
+  effectRunner = scriptModules.effectRunner;
   eventManager = scriptModules.eventManager;
-  variableManager = scriptModules.replaceVariableManager;
+  httpServer = scriptModules.httpServer;
   integrationManager = scriptModules.integrationManager;
   jsonDb = scriptModules.JsonDb;
+  logger = scriptModules.logger;
+  twitchApi = scriptModules.twitchApi;
+  twitchChat = scriptModules.twitchChat;
   utils = scriptModules.utils;
-  httpServer = scriptModules.httpServer;
+  variableManager = scriptModules.replaceVariableManager;
 }
